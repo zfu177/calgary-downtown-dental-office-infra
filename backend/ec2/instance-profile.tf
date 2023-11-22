ff065b62cc6a60f45d92d94bb9b52266fe88513f
Zhongyi Fu <z.fu177@mybvc.ca>
2023-11-22 13:45:13 -0700

    update iam instance profile

--- backend/ec2/instance-profile.tf
+++ backend/ec2/instance-profile.tf
@@ -44,5 +44,5 @@ resource "aws_iam_role" "role" {
   name                = "${var.service_name}-role"
   path                = "/"
   assume_role_policy  = data.aws_iam_policy_document.assume_role.json
-  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"]
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM", "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"]
 }
